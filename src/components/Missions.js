b3f5a20c4cb160d6425f412bdb8f36c214df5566
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-25 16:42:49 -0300

    Requisito 7 - Completo

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <p> Oi, eu estou aqui!</p>
+      </div>
+    );
+  }
+}
+
+export default Missions;
